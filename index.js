f5e6ef90ce85ef98ce8c603c9b8df488895cd16a
Shriram Ravi <shriramravi96@gmail.com>
2022-04-16 01:59:46 -0400

    index js update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static('src'));
-app.use(express.static('../build/contracts'));
+app.use(express.static('build/contracts'));
+app.use(express.static('node_modules'));
 app.get('/', function (req, res) {
   res.render('index.html');
 });

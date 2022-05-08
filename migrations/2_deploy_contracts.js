baba77991e13b302f9fabd7be0fbafe0b2764aa9
Shriram Ravi <shriramravi96@gmail.com>
2022-05-08 13:01:12 -0400

    contract deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const MeloManiac = artifacts.require("MeloManiac");
+const MeloNotes = artifacts.require("MeloNotes");
 
 module.exports = function (deployer) {
-  deployer.deploy(MeloManiac);
+  deployer.deploy(MeloNotes, "MeloNotes", "MNT", 200).then(function() {
+    return deployer.deploy(MeloManiac, MeloNotes.address);
+  });
 };

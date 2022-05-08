965cd11d5ef5e9fea941eacec5dbb4b4306016bd
Shriram Ravi <shriramravi96@gmail.com>
2022-05-08 19:21:40 -0400

    Truffle config for infura ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -8,6 +10,11 @@ module.exports = {
       port: 7545,
       network_id: "5777",
       gas: 4600000
+    },
+    inf_MeloManiac_ropsten: {
+      network_id: 3,
+      gasPrice: 100000000000,
+      provider: new HDWalletProvider(fs.readFileSync('/tmp/mnemonic.env', 'utf-8'), "https://ropsten.infura.io/v3/62cff11104f54085984676c0008ebce0", 2)
     }
   },
   compilers: {

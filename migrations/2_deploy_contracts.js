8b0514745b915b5010cfda73a1fc2975b11f8412
Shriram Ravi <shriramravi96@gmail.com>
2022-05-08 17:59:16 -0400

    Change number of minted tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const MeloManiac = artifacts.require("MeloManiac");
 const MeloNotes = artifacts.require("MeloNotes");
 
 module.exports = function (deployer) {
-  deployer.deploy(MeloNotes, "MeloNotes", "MNT", 200).then(function() {
+  deployer.deploy(MeloNotes, "MeloNotes", "MNT", 2000000000000).then(function() {
     return deployer.deploy(MeloManiac, MeloNotes.address);
   });
 };
